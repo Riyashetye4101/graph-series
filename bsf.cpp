30a83f36a4810bc08fdbbe27449087209e78936f
Riyashetye4101 <84229593+Riyashetye4101@users.noreply.github.com>
2023-01-13 16:31:45 +0530

    Create bsf.cpp

---
+++ bsf.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#include<utility>
+using namespace std;
+
+void bfs(int startnode,vector<int> *adj,vector<int> &ans,int n){
+	queue<int> q;
+	vector<bool> visited(n+1,false);
+	q.push(startnode);
+    
+	while(!q.empty()){
+
+		int t=q.front();
+		q.pop();
+		if(!visited[t]){
+            ans.push_back(t);
+            visited[t]=true;
+            
+            for(int i:adj[t]){
+            	q.push(i);
+            }
+		}
+	}
+
+
+}
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<int> adj[n+1];
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<int> ans;
+    bfs(5,adj,ans,n);
+    for(int i:ans){
+    	cout<<i<<" ";
+    }
+    return 0;
+    
+}
